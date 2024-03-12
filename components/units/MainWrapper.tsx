bcf4a6fe180051aab6ac6a39b7a8657c9805b27b
alstjd0051 <wsc03002@naver.com>
2024-03-12 22:20:46 +0900

    Add MainWrapper component

---
+++ components/units/MainWrapper.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+type Props = {};
+
+const MainWrapper = (props: Props) => {
+  return <section className="h-dvh"></section>;
+};
+
+export default MainWrapper;
