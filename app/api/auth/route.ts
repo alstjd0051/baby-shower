e071b848f2e752510f7873dc12802f3b46c83304
alstjd0051 <wsc03002@naver.com>
2024-03-12 22:20:10 +0900

    Add authentication route and social login icons

---
+++ app/api/auth/route.ts
@@ -0,0 +1,29 @@
+import { client } from "@/components/lib/sanity/user";
+import { groq } from "next-sanity";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  const { id } = await req.json();
+
+  const querys = groq`*[_type =='author' && _id == "${id}" ]|order(_createdAt desc){
+  "id": _id,
+  image,
+  username,
+  name,
+  email,
+  phone
+  }[0]
+  `;
+
+  try {
+    const res = await client.fetch(querys, {}, { cache: "no-store" });
+
+    return NextResponse.json(res, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+};
+
+export const GET = async (req: Request) => {
+  const res = client.fetch(`*[_type == "author"]`);
+};
---
+++ components/assets/socialLogin.ts
@@ -0,0 +1,12 @@
+export const Socials = [
+  {
+    name: "Google",
+    icon: "https://img.icons8.com/color/48/000000/google-logo.png",
+    url: "google",
+  },
+  {
+    name: "Naver",
+    icon: "https://m-logoproject.naver.com/img/naver_bi.png",
+    url: "naver",
+  },
+];
