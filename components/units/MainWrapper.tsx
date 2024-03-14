9937f6c38fa0c64cfaac88e0af53c77f7c64af6f
alstjd0051 <wsc03002@naver.com>
2024-03-15 07:53:59 +0900

    Add MainImages component and update MainWrapper with loading state

--- components/units/MainWrapper.tsx
+++ components/units/MainWrapper.tsx
@@ -1,17 +1,34 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RightTiele from "../commons/items/framer/rightTiele";
 import { CakeLoading } from "../commons/items/spinner/CakeLoading";
 import { useMainSorage } from "../hooks/admin/storage";
+import MainImages from "../commons/items/main/images";
 
 type Props = {};
 
 const MainWrapper = (props: Props) => {
   const { getAdmin, getLoading } = useMainSorage("main");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 10000);
+
+    if (loading) {
+      timer;
+    }
+    return () => clearTimeout(timer);
+  }, [loading]);
 
   return (
-    <section className="h-dvh">
-      <CakeLoading getAdmin={getAdmin} getLoading={getLoading} />
+    <section className="h-dvh relative">
+      {loading ? (
+        <CakeLoading />
+      ) : (
+        <MainImages getAdmin={getAdmin} getLoading={getLoading} />
+      )}
       <RightTiele />
     </section>
   );
